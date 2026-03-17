3f49301ebe684efd87707052b660a047144170ef
lyns0 <disse.sl@proton.me>
2026-03-17 10:25:30 +0700

    feat: add tests for Queue

---
+++ firefly_core/src/queue/tests.rs
@@ -0,0 +1,54 @@
+use std::path::PathBuf;
+
+use crate::{queue::Queue, track::Track};
+
+#[test]
+fn next() {
+    let mut queue = Queue::default();
+    let pathbuf_1 = PathBuf::from("../test_assets/test.flac");
+    let pathbuf_2 = PathBuf::from("../test_assets/test1.flac");
+
+    queue.enqueue(Track::new(pathbuf_1.as_path()).unwrap());
+    queue.enqueue(Track::new(pathbuf_2.as_path()).unwrap());
+
+    assert!(queue.next());
+    assert_eq!(queue.current_track.as_ref().unwrap().pathbuf, pathbuf_1);
+
+    assert!(queue.next());
+    assert!(queue.current_track.is_some());
+    assert_eq!(queue.current_track.as_ref().unwrap().pathbuf, pathbuf_2);
+
+    assert!(!queue.next());
+    assert_eq!(queue.current_track.as_ref().unwrap().pathbuf, pathbuf_2);
+}
+
+#[test]
+fn prev() {
+    let mut queue = Queue::default();
+    let pathbuf_1 = PathBuf::from("../test_assets/test.flac");
+    let pathbuf_2 = PathBuf::from("../test_assets/test1.flac");
+
+    queue.enqueue(Track::new(pathbuf_1.as_path()).unwrap());
+    queue.enqueue(Track::new(pathbuf_2.as_path()).unwrap());
+
+    assert!(!queue.prev());
+    assert!(queue.current_track.is_none());
+
+    queue.next();
+    queue.next();
+
+    assert!(queue.prev());
+    assert_eq!(queue.current_track.unwrap().pathbuf, pathbuf_1);
+}
+
+#[test]
+fn prepend() {
+    let mut queue = Queue::default();
+    let pathbuf_1 = PathBuf::from("../test_assets/test.flac");
+    let pathbuf_2 = PathBuf::from("../test_assets/test1.flac");
+
+    queue.enqueue(Track::new(pathbuf_1.as_path()).unwrap());
+    queue.prepend_queue(Track::new(pathbuf_2.as_path()).unwrap());
+    queue.next();
+    assert_eq!(queue.current_track.unwrap().pathbuf, pathbuf_2);
+}
