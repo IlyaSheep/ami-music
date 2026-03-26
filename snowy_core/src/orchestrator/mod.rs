ce93436937e5580a74dd9c8645f0ecb4b2f8174a
lyns0 <disse.sl@proton.me>
2026-03-26 15:27:58 +0700

    feat: implement new() for Orchestrator

--- snowy_core/src/orchestrator/mod.rs
+++ snowy_core/src/orchestrator/mod.rs
@@ -1,6 +1,17 @@
+use anyhow::Result;
+
 use crate::{player::Playback, queue::Queue};
 
 pub struct Orchestrator {
     player: Playback,
     queue: Queue,
+}
+
+impl Orchestrator {
+    pub fn new() -> Result<Self> {
+        Ok(Orchestrator {
+            player: Playback::new()?,
+            queue: Queue::default(),
+        })
+    }
 }
