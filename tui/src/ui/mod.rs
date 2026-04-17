89d6327e7f00a801e30a4de61f4378fb9e37a32e
lyns0 <disse.sl@proton.me>
2026-04-17 13:50:34 +0700

    TUI feat: add now_playing ui component

---
+++ tui/src/ui/mod.rs
@@ -0,0 +1,27 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::Widget,
+};
+
+use crate::{app::App, ui::now_playing::NowPlaying};
+
+pub mod now_playing;
+
+impl Widget for &App {
+    /// Renders the user interface widgets.
+    ///
+    // This is where you add new widgets.
+    // See the following resources:
+    // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
+    // - https://github.com/ratatui/ratatui/tree/master/examples
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
+            .split(area);
+
+        let now_playing = NowPlaying { app: &self };
+        now_playing.render(layout[0], buf);
+    }
+}
---
+++ tui/src/ui/now_playing.rs
@@ -0,0 +1,49 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::{Color, Stylize},
+    widgets::{Block, BorderType, Paragraph, Widget},
+};
+
+use crate::app::App;
+
+pub struct NowPlaying<'a> {
+    pub app: &'a App,
+}
+
+impl<'a> Widget for NowPlaying<'a> {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        if let Some(track) = self
+            .app
+            .states
+            .blocking_lock()
+            .queue_snapshot
+            .current_track
+            .as_ref()
+        {
+            let block = Block::bordered()
+                .title("test")
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded);
+
+            let text = format!(
+                "{}\n\
+                            Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+                            Press left and right to increment and decrement the counter respectively.\n\
+                        ",
+                track.metadata.title.as_ref().unwrap_or(&"No Title".into())
+            );
+
+            let paragraph = Paragraph::new(text)
+                .block(block)
+                .fg(Color::Cyan)
+                .bg(Color::Black)
+                .centered();
+
+            paragraph.render(area, buf);
+        }
+    }
+}
