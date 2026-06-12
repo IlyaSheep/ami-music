612d9fb6217f2db435905aa5984bec53258b245a
lyns0 <disse.sl@proton.me>
2026-06-12 10:17:43 +0700

    CORE feat: serialize TrackId as string

--- core/src/track/track_id.rs
+++ core/src/track/track_id.rs
@@ -5,11 +5,13 @@ use std::{
 };
 
 use serde::{Deserialize, Serialize};
+use serde_with::{DisplayFromStr, serde_as};
 use ts_rs::TS;
 
+#[serde_as]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize, TS)]
-#[ts(export, export_to = "track_id.ts")]
-pub struct TrackId(pub u64);
+#[ts(export, export_to = "track_id.ts", type = "string")]
+pub struct TrackId(#[serde_as(as = "DisplayFromStr")] pub u64);
 
 impl TrackId {
     pub fn from_path(path: &Path) -> TrackId {
