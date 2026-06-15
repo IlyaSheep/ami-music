40ac063890efe28156e0a4b39154a3102002c258
lyns0 <disse.sl@proton.me>
2026-06-15 14:53:03 +0700

    WEB feat: create DaemonStates and connect()

---
+++ web-ui/src/lib/ws.ts
@@ -0,0 +1,34 @@
+import type { Command } from "../types/commands"
+import type { ServerEvent } from "../types/server_event"
+import { daemonState } from "./stores/daemon_states"
+
+let ws: WebSocket | null = null
+
+
+export function connect(url: string) {
+  ws = new WebSocket(url)
+  ws.onmessage = (e) => {
+    const msg: ServerEvent = JSON.parse(e.data)
+
+    switch (msg.type) {
+      case "SendLibrary":
+        daemonState.library = msg.data
+        break
+      case "SendQueue":
+        daemonState.queue = msg.data
+        break
+      case "SendPlayerSnapshot":
+        daemonState.player = msg.data
+        break
+      case "SendPlayerPosition":
+        if (daemonState.player) {
+          daemonState.player.position = msg.data.position
+        }
+        break
+    }
+  }
+}
+
+export function send(msg: Command) {
+  ws?.send(JSON.stringify(msg))
+}
